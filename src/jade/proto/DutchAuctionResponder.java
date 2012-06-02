6bf1ff510cdf57e37791c01fdabfe216e91beaae
Anca <anca.balanel@gmail.com>
2012-06-02 12:32:59 +0300

    responder stubs

---
+++ src/jade/proto/DutchAuctionResponder.java
@@ -0,0 +1,15 @@
+package jade.proto;
+
+import jade.core.Agent;
+import jade.core.behaviours.DataStore;
+import jade.lang.acl.ACLMessage;
+
+public class DutchAuctionResponder extends SSResponder {
+
+	public DutchAuctionResponder(Agent a, ACLMessage initiation,
+			DataStore store, boolean useInitiationKey) {
+		super(a, initiation, store, useInitiationKey);
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ src/jade/proto/EnglishAuctionResponder.java
@@ -0,0 +1,15 @@
+package jade.proto;
+
+import jade.core.Agent;
+import jade.core.behaviours.DataStore;
+import jade.lang.acl.ACLMessage;
+
+public class EnglishAuctionResponder extends SSResponder {
+
+	public EnglishAuctionResponder(Agent a, ACLMessage initiation,
+			DataStore store, boolean useInitiationKey) {
+		super(a, initiation, store, useInitiationKey);
+		// TODO Auto-generated constructor stub
+	}
+
+}
